23634dc8fff4a1195873e4e0ed50d15ec17cdda2
akshat1032 <akshat.30oct@gmail.com>
2020-10-03 14:08:35 +0530

    Added : Show the game board added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -12,29 +12,33 @@ public class TicTacToeGame {
 		}
 		return ticTacToeBoard;
 	}
-	
-	//Allow player to choose a letter X or O
+
+	// Allow player to choose a letter X or O
 	private char chooseLetter(Scanner userInputLetter) {
 		System.out.println("Enter your choice either X or O");
 		char userChoice = userInputLetter.next().toUpperCase().charAt(0);
 		return userChoice;
 	}
-	
+
+	// Display the board
+	private void showBoard(char gameBoard[]) {
+		for(int indexBoard = 1; indexBoard<gameBoard.length; indexBoard++) {
+			System.out.print(gameBoard[indexBoard]+" |");
+			if(indexBoard % 3 == 0 )
+				System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe game");
-		
+
 		char ticTacToeBoard[] = new char[10];
 		TicTacToeGame gameObject = new TicTacToeGame();
 		ticTacToeBoard = gameObject.createBoard();
-		
+
 		Scanner userInputLetter = new Scanner(System.in);
 		char userChoice = gameObject.chooseLetter(userInputLetter);
-		char computerChoice;
-		if (userChoice == 'O') {
-			computerChoice = 'X';
-		} else {
-			computerChoice = 'O';
-		}
+		char computerChoice = ((userChoice == 'X')?'O':'X');
+		gameObject.showBoard(ticTacToeBoard);
 	}
 }
