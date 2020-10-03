22d346a2f67167e4521c0a06e946f6a86217a57e
akshat1032 <akshat.30oct@gmail.com>
2020-10-03 13:26:33 +0530

    Refactor : Tic Tac Toe game board creation refactored

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,21 +1,20 @@
 package com.tictactoegame;
 
 public class TicTacToeGame {
-	
+
 	private char ticTacToeBoard[];
-	
+
+	// Create game board and assign empty space to each index
 	public void createBoard() {
-		
 		ticTacToeBoard = new char[10];
-		for(int i = 0; i<10; i++) {
-			ticTacToeBoard[i] = ' ';
+		for (int indexBoard = 0; indexBoard < ticTacToeBoard.length; indexBoard++) {
+			ticTacToeBoard[indexBoard] = ' ';
 		}
 	}
 
 	public static void main(String[] args) {
-	
 		System.out.println("Welcome to Tic Tac Toe game");
-		
+
 		TicTacToeGame gameObject = new TicTacToeGame();
 		gameObject.createBoard();
 	}
