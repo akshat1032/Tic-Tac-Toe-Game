11eaa38154488fb6f5416cb739616887d3d98402
akshat1032 <akshat.30oct@gmail.com>
2020-10-03 13:03:22 +0530

    Added : Tic Tac Toe game board creation added

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,9 +1,22 @@
 package com.tictactoegame;
 
 public class TicTacToeGame {
+	
+	private char ticTacToeBoard[];
+	
+	public void createBoard() {
+		
+		ticTacToeBoard = new char[10];
+		for(int i = 0; i<10; i++) {
+			ticTacToeBoard[i] = ' ';
+		}
+	}
 
 	public static void main(String[] args) {
 	
 		System.out.println("Welcome to Tic Tac Toe game");
+		
+		TicTacToeGame gameObject = new TicTacToeGame();
+		gameObject.createBoard();
 	}
 }
